3403d5a2137fc3d0d767d1016a2c6b969be972a8
elisha johnsunder <ejohn3@umbc.edu>
2017-03-01 20:16:26 -0500

    Create proxyLoader.py

---
+++ proxyLoader.py
@@ -0,0 +1,83 @@
+import requests
+from bs4 import BeautifulSoup
+from OpenSSL import SSL
+from termcolor import cprint
+import json
+
+def filterConnections(proxiesList):
+	workingProxies = []
+	count = 0
+	for proxy in proxiesList:
+		count += 1
+		cprint("Loading proxy # {}".format(count), "green")
+		proxies = {
+		  'http': proxy,
+		  'https': proxy
+		}
+		try:
+			r = requests.get("http://www.supremenewyork.com/shop/all", proxies=proxies, timeout=.1)
+			data = r.text
+			soup = BeautifulSoup(data,"html.parser")
+			headerCheck = str(soup.find("span",{"id":"time-zone-name"}).text)
+			if headerCheck == "NYC":
+				cprint(headerCheck, "blue")
+				workingProxies.append(proxy)
+				cprint("Added {}!".format(proxy),"green")
+			else:
+				cprint("Banned!", "red")
+				raise
+		except:
+			cprint("Bad Proxy: {}".format(proxy), "red")
+	return workingProxies
+
+def site1():
+	url = "http://www.aliveproxy.com/fastest-proxies/"
+	user = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"}
+	r = requests.get(url,headers=user)
+
+	data = r.text
+	soup = BeautifulSoup(data,"html.parser")
+	for ips in soup.find_all("tr",{"class":"cw-list"}):
+		for ip in ips.find_all("td",{"class":"dt-tb2"}):
+			print(ip)
+
+			break;
+
+def site2(proxiesList):
+	url = "https://www.us-proxy.org/"
+	user = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"}
+	r = requests.get(url,headers=user)
+
+	data = r.text
+	soup = BeautifulSoup(data,"html.parser")
+
+	table = soup.find("tbody")
+	for ips in table.find_all("tr"):
+		count = 0
+		proxy = ""
+		for ip in ips.find_all("td"):
+			if count == 0:
+				proxy = str(ip.text)
+				proxy += ":"
+			if count == 1:
+				proxy += str(ip.text)
+				proxiesList.append(proxy)
+				break;
+			count += 1
+	cprint("Succesfully added {} proxies!".format(len(proxiesList)), 'green')
+
+def loadProxies():
+	proxiesList = []
+	cprint("Loading proxies...","green")
+
+	site2(proxiesList) # load proxies
+
+	proxiesList = filterConnections(proxiesList) # filter for working connections
+
+	# Write to file
+	with open("proxies.txt", 'w') as outfile:
+		json.dump(proxiesList, outfile)
+	cprint("Proxies saved to proxies.txt!","magenta","on_grey", attrs=['bold'])
+
+
+

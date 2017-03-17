c177d1f3688460a92ee8d4240660a950e337a8d4
elisha johnsunder <ejohn3@umbc.edu>
2017-03-17 01:47:09 -0400

    uses main site now!

--- monitor.py
+++ monitor.py
@@ -15,18 +15,21 @@ import sys
 # Use masterStock and insert ID's to monitor
 IDs = {'170462':'Denim Logo Chore Coat','170471':'Supreme/LACOSTE Track Jacket','170474':'Supreme/LACOSTE Harrington Jacket','170464':'Polka Dot S/S Shirt','170463':'Curve Logo Tee','170469':'Supreme/LACOSTE L/S Jersey Polo','170473':'Supreme/LACOSTE Tennis Sweater','170465':'666 Zip Up Sweat','170466':'Sequin Logo Hooded Sweatshirt','170468':'Supreme/LACOSTE Pique Crewneck','170472':'Supreme/LACOSTE Track Pant','170470':'Supreme/LACOSTE Pique Short','170460':'Leather Camp Cap','170461':'Skew Nylon 5-Panel','170467':'Supreme/LACOSTE Pique Camp Cap','170459':'Studded Belt','170462':'Denim Logo Chore Coat','170471':'Supreme/LACOSTE Track Jacket','170474':'Supreme/LACOSTE Harrington Jacket','170464':'Polka Dot S/S Shirt','170463':'Curve Logo Tee','170469':'Supreme/LACOSTE L/S Jersey Polo','170473':'Supreme/LACOSTE Tennis Sweater','170465':'666 Zip Up Sweat','170466':'Sequin Logo Hooded Sweatshirt','170468':'Supreme/LACOSTE Pique Crewneck','170472':'Supreme/LACOSTE Track Pant','170470':'Supreme/LACOSTE Pique Short','170460':'Leather Camp Cap','170461':'Skew Nylon 5-Panel','170467':'Supreme/LACOSTE Pique Camp Cap','170459':'Studded Belt'}
 
+# IDs = {'170461':'Skew Nylon 5-Panel','170427':'Skew Nylon 5-Panel'}
 
 # Get this from proxies.txt
 proxyList = [""]
+
 stock = {}
 
-def sendTweet(item,color,link):
+def sendTweet(item,color,link, size):
 	auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 	auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 	api = tweepy.API(auth)
 
 	tweet = item+"\n"
 	tweet += color+'\n'
+	tweet += size.title()+'\n'
 	tweet += link+'\n'
 	tweet += "Restock!"+'\n'
 	tweet += str(datetime.utcnow().strftime('%H:%M:%S.%f')[:-3])
@@ -48,62 +51,63 @@ def getIp(proxy):
 def compareStock():
 	global IDs
 	global stock
+	sizeKey = ['OS','SMALL','MEDIUM','LARGE','XLARGE','S/M','L/XL'] # Add more if necessary
 	with open("stock.txt", 'r') as outfile:
 		oldStock = json.load(outfile)
 
 	# For testing:
 	# stock['170423']['Peach'][1] = 1
-	# stock['170442']['Turquoise'][0] = 0
-
+	# stock['170427']['Peach']['sizes']['LARGE'] = 0
+	# stock['170462']['Red']['sizes']['MEDIUM'] = 1
 	change = 0
 	for ID in IDs.keys():
 		try:
 			for color in stock[ID]:
-				# Handle one size
-				sizeLen = len(stock[ID][color]) - 1
-				sizeLenCheck = False
-				if sizeLen == 0:
-					sizeLen = 1
-					sizeLenCheck = True
-
-				for size in range(0,sizeLen):
-					# print("diff {} : {}".format(stock[ID][color][size],oldStock[ID][color][size]))
-					if (stock[ID][color][size] == 1) and (oldStock[ID][color][size] == 0):
-						print("Restock!")
-						item = IDs[ID]
-						link = "http://www.supremenewyork.com/shop/"+str(ID)+"/"+str(ID)
-						sendTweet(item,color,link)
-						# print("\n")
-						# cprint(item,"green")
-						# cprint(color,"blue")
-						# cprint(link,"red")
-						# print("\n")
+				for size in sizeKey:
+					# If size not in stock restocked!
+					if size in stock[ID][color]['sizes'].keys() and size not in oldStock[ID][color]['sizes'].keys():
 						change = 1
+						# print("New size!"+ str(stock[ID][color]['sizes'][size]))
+						oldStock[ID][color]['sizes'][size] = stock[ID][color]['sizes'][size]
+						cprint("New size restock!","yellow")
+						item = IDs[ID]
+						itemColor = stock[ID][color]['id']
+						link = "http://www.supremenewyork.com/shop/"+"supszn/"+str(ID)+"/"+str(itemColor)
+						itemSize = size
+						sendTweet(item,color,link, itemSize)
 
-					elif (stock[ID][color][size] == 0) and (oldStock[ID][color][size] == 1):
-						print("OOS!")
-						stock[ID][color][size] = 2
+			for size in stock[ID][color]['sizes']:
+				if (stock[ID][color]['sizes'][size] == 1) and (oldStock[ID][color]['sizes'][size] == 0):
+					item = IDs[ID]
+					itemColor = stock[ID][color]['id']
+					link = "http://www.supremenewyork.com/shop/"+"supszn/"+str(ID)+"/"+str(itemColor)
+					itemSize = size
+					sendTweet(item,color,link, itemSize)
+					change = 1
+
+				elif (stock[ID][color]['sizes'][size] == 0) and (oldStock[ID][color]['sizes'][size] == 1):
+					stock[ID][color]['sizes'][size] = 2
+					with open("stock.txt", 'w') as outfile:
+						json.dump(stock, outfile)
+
+
+				if (oldStock[ID][color]['sizes'][size] > 1):
+						stock[ID][color]['sizes'][size] = oldStock[ID][color]['sizes'][size] + 1
+						cprint(ID+" - OOS! ++ "+str(stock[ID][color]['sizes'][size]),"yellow")
 						with open("stock.txt", 'w') as outfile:
 							json.dump(stock, outfile)
 
-					if (oldStock[ID][color][size] > 1):
-						stock[ID][color][size] = oldStock[ID][color][size] + 1
-						cprint(ID+" - OOS! ++ "+str(stock[ID][color][size]),"yellow")
-						with open("stock.txt", 'w') as outfile:
-							json.dump(stock, outfile)
-
-					if (oldStock[ID][color][size] == 300):
+				if (oldStock[ID][color]['sizes'][size] == 300):
 						print("OOS! MAX ")
-						stock[ID][color][size] = 0
+						stock[ID][color]['sizes'][size] = 0
 						cprint(IDs[ID],"red")
 						cprint("OOS","red")
 						change = 1
-
-					if sizeLenCheck:
-						break
 		except:
 			print(ID)
+			print(oldStock[ID][color]['sizes'])
 			cprint("Invalid ID","red")
+
 
 	if change == 1:
 		with open("stock.txt", 'w') as outfile:
@@ -116,7 +120,8 @@ def restockCheck(sku, extra):
 	global stock
 
 
-	url = "http://www.supremenewyork.com/shop/" + str(sku) +".json"
+	url = "http://www.supremenewyork.com/shop/" + str(sku) + "/" + str(sku)
+	urlJson = "http://www.supremenewyork.com/shop/" + str(sku) +".json"
 	user = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"}
 
 	success = False
@@ -134,7 +139,8 @@ def restockCheck(sku, extra):
 		try:
 			cprint("Loading proxy: {}".format(ip),"blue")
 			r = requests.get(url, headers=user, proxies=proxies, timeout=2)
-			newStock = json.loads(r.text)
+			r2 = requests.get(urlJson, headers=user, proxies=proxies, timeout=2)
+			newStock = json.loads(r2.text)
 			success = True
 			break # just incase lol
 		except:
@@ -143,14 +149,35 @@ def restockCheck(sku, extra):
 			if count == (len(proxyList) - 1):
 				exit()
 
+	data = r.text
+	soup = BeautifulSoup(data,"html.parser")
 
 	colorDict = {}
 	for color in newStock['styles']:
-		sizeStock = []
-		for size in color['sizes']:
-			sizeStock.append(size['stock_level'])
+		sizeStock = {}
+		sizeStock["id"] = color['id']
+		stockDict = {}
+		status = str(soup.find(id="add-remove-buttons"))
+		if "add to cart" in status:
+			# Get sizes
+			sizes = soup.find(id="size")
+			if sizes.find('option') == None:
+				# print("OS : "+sizes.get('value'))
+				stockDict['OS'] = 1
+			else:
+				for size in sizes.find_all('option'):
+					# print(size.text + " : " +size.get('value'))
+					stockDict[str(size.text).upper()] = 1
+
+		elif "sold out" in status:
+			stockDict['None'] = 0
+		sizeStock['sizes'] = stockDict
+
 		colorDict[color['name']] = sizeStock
+
 	stock[sku] = colorDict
+
+
 
 def multiCheck(items):
 	with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
@@ -186,11 +213,12 @@ def main(argv):
 		# If you don't want threading: 
 		# for ID in IDs.keys():
 		# 	print("Checking: {}".format(ID))
-		# 	restockCheck(ID , proxy, 1)
+		# 	restockCheck(ID, 1)
 		# 	time.sleep(.5)
 
 		# Use threading:
 		multiCheck(list(IDs.keys()))
+		# print(stock)
 		if stock:
 			compareStock()
 	except:

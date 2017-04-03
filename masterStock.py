fec94ede05c77adcdab405b74ef8a1d08fb7b73c
elisha johnsunder <ejohn3@umbc.edu>
2017-04-02 21:50:18 -0400

    Update masterStock.py

--- masterStock.py
+++ masterStock.py
@@ -8,40 +8,39 @@ def loadMasterStock():
 	# user = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"}
 	r = requests.get(url, headers=user)
 	masterStock = json.loads(r.text)
-	with open("masterstock.txt", 'w') as outfile:
+	with open("masterstock.json", 'w') as outfile:
 		json.dump(masterStock, outfile, indent=4, sort_keys=True)
 
-	print("Saved to masterstock.txt")
+	print("Saved to masterstock.json")
+
 	itemInfo = ""
-
-
 	while(True):
 		try:
-			change = 1
-			item = input("Enter item name to get id: ")
-			if itemInfo != "":
-				itemInfo += ","
+			item = input("Enter item name to get id or cntrl-c to quit: ")
 		except:
 			print("Exiting...")
+			if itemInfo != "":
+				itemInfo = itemInfo[:-1]
 			print("\n"+itemInfo)
+
+			with open("filteredStock.json",'w') as outfile:
+				outfile.write("filteredStock = {\n")
+				outfile.write(itemInfo)
+				outfile.write("\n}")
+			print("\nSaved to filteredStock.json...")
 			exit()
-		for itemCount in range(len(masterStock['products_and_categories']["new"])):
-			if item.lower() == "new":
-				change = 0
-				itemInfo += "'"+str(masterStock['products_and_categories']["new"][itemCount]['id'])+"':'"
-				itemInfo += str(masterStock['products_and_categories']["new"][itemCount]['name'])+"'"
-
-			elif item.lower() in str(masterStock['products_and_categories']["new"][itemCount]['name']).lower():
-				change = 0
-				itemInfo += "'"+str(masterStock['products_and_categories']["new"][itemCount]['id'])+"':'"
-				itemInfo += str(masterStock['products_and_categories']["new"][itemCount]['name'])+"'"
-				print("Added to list...")
-
-		if change:
-			print("Item not found!")
-
-
-
+		if item == "new":
+			print("Getting all new items...")
+			for itemCount in range(len(masterStock['products_and_categories']["new"])):
+					itemInfo += '"'+str(masterStock['products_and_categories']["new"][itemCount]['id'])+'":"'
+					itemInfo += str(masterStock['products_and_categories']["new"][itemCount]['name'])+'",'
+		else:
+			for itemCount in range(len(masterStock['products_and_categories']["new"])):
+				if item.lower() in str(masterStock['products_and_categories']["new"][itemCount]['name']).lower():
+					itemInfo += '"'+str(masterStock['products_and_categories']["new"][itemCount]['id'])+'":"'
+					print("Added "+str(masterStock['products_and_categories']["new"][itemCount]['name']))
+					itemInfo += str(masterStock['products_and_categories']["new"][itemCount]['name'])+'",'
+					# print(itemInfo)
 
 if __name__ == '__main__':
 	loadMasterStock()
